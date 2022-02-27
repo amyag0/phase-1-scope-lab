420929be4f82770d66c5c279b6af0bc76c067b3f
amyag0 <amyago19@gmail.com>
2022-02-26 21:07:14 -0500

    commit tree c38d5fa2f33f9587973f985a5678199f666c4318

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";//test 1
+
+function upperCaseCustomerName(){ //test2
+    customerName = customerName.toUpperCase(); 
+}
+
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+
+
+function overwriteBestCustomer(){
+    setBestCustomer();
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Bob";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bobs friend";
+
+}
